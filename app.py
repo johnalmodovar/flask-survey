73030c0ae4aa6006b1e5940e410e599fc8203d64
Michael <michaellngriffin@gmail.com>
2023-08-23 23:33:15 -0400

    survey gets to the first question, answer redirect needs work

--- app.py
+++ app.py
@@ -8,30 +8,73 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+responses = []
+#surveys = {"satisfaction: satisfaction_survey",
+# "personality" : personality_quiz
+# }
+
+
 @app.get("/")
 def show_start_page():
     """ Generates title of survey, its instructions, and the start button """
 
-    survey_title = satisfaction_survey.title
-    survey_instructions = satisfaction_survey.instructions
+    survey_title = survey.title
+    survey_instructions = survey.instructions
 
     return render_template("survey_start.html",
                            title = survey_title,
                            instructions = survey_instructions)
 
-@app.post("/")
+@app.post("/begin")
 def redirect_to_survey():
     """ Click the start button to redirect to survey questions """
 
     return redirect("/questions/0")
 
-@app.get("/questions/<num>")
-def load_questions():
+@app.get("/questions/<int:num>")
+def load_questions(num):
     """ Load questions from the survey """
 
-    survey_question = satisfaction_survey.questions["num"]
+    # self.prompt = prompt
+    # self.choices = choices
+    # self.allow_text = allow_text
+    print(f"\n \n This is num : {num} \n \n")
+
+    survey_question = survey.questions[num]
+
+    return render_template("question.html",
+                           prompt = survey_question.prompt,
+                           choices = survey_question.choices,
+                           allow_text = survey_question.allow_text)
 
 
 
+@app.post("/questions/<num>")
+def submit_questions():
+    """Submit survey response, load next question."""
 
-responses = []
+    curr_answer = request.form["answer"]
+    responses.append(curr_answer)
+    redirect("/answer")
+
+
+@app.get('/answer')
+def receive_answer():
+    """When submitting an answer, grab from request.form, append to response,
+    then send them to the next question."""
+
+    if len(responses) >= len(survey.questions):
+        redirect_url = "/completed"
+    else:
+        redirect_url = f"/questions/{len(responses)}"
+
+    redirect(redirect_url)
+
+
+@app.get('/completed')
+def show_survey_finish():
+    """Provide a bulleted list of question + response"""
+
+    return render_template('completion.html',
+                           questions= survey.questions, #actually want prompt
+                           answers = responses)
