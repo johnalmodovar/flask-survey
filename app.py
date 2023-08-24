0d897632d3c43dd9b94199a7e2f4f29b5905b002
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-08-24 11:29:12 -0700

    made view functions for root page and to redirect to survey questions

--- app.py
+++ app.py
@@ -7,3 +7,31 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def show_start_page():
+    """ Generates title of survey, its instructions, and the start button """
+
+    survey_title = satisfaction_survey.title
+    survey_instructions = satisfaction_survey.instructions
+
+    return render_template("survey_start.html",
+                           title = survey_title,
+                           instructions = survey_instructions)
+
+@app.post("/")
+def redirect_to_survey():
+    """ Click the start button to redirect to survey questions """
+
+    return redirect("/questions/0")
+
+@app.get("/questions/<num>")
+def load_questions():
+    """ Load questions from the survey """
+
+    survey_question = satisfaction_survey.questions["num"]
+
+
+
+
+responses = []
